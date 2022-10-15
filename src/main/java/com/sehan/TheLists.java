9b2f67e85762101379f9387bc49857d4307c7fce
sehan1 <sehan@openarcgroup.com>
2022-10-15 18:00:59 +0530

    The List

---
+++ src/main/java/com/sehan/TheLists.java
@@ -0,0 +1,26 @@
+package com.sehan;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TheLists {
+    public static void main(String[] args) {
+        List<String> colors = new ArrayList<>();
+        colors.add("blue");
+        colors.add("purple");
+        colors.add("yellow");
+        System.out.println(colors.size());
+        System.out.println(colors.contains("yellow"));
+        System.out.println(colors.contains("pink"));
+        System.out.println(colors);
+
+        for (String color : colors){
+            System.out.println(color);
+        }
+
+        colors.forEach(System.out::println);
+        for (int i = 0; i < colors.size(); i++) {
+            System.out.println(colors.get(i));
+        }
+    }
+}
