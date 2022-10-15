e45a9e06189100be79c42ec5cd5f7ee2893be09a
sehan1 <sehan@openarcgroup.com>
2022-10-15 21:18:59 +0530

    The Queues

---
+++ src/main/java/com/sehan/TheQueues.java
@@ -0,0 +1,20 @@
+package com.sehan;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class TheQueues {
+    public static void main(String[] args) {
+        Queue<Person> queue = new LinkedList<>();
+        queue.add(new Person("Alex", 21));
+        queue.add(new Person("Mariam", 18));
+        queue.add(new Person("Ali", 40));
+
+        System.out.println(queue.size());
+        System.out.println(queue.peek());
+        System.out.println(queue.poll());
+        System.out.println(queue.size());
+        System.out.println(queue.peek());
+    }
+    static record Person(String name, int age){}
+}
