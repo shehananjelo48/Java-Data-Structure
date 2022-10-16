53d2873943f725405dc30475612fe6a9606bf81d
sehan1 <sehan@openarcgroup.com>
2022-10-16 18:32:24 +0530

    The LinkedList

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,9 @@
+
+X
+(src/main/java/com/sehan/The2DArrays.java,5\f\5fcf93d9bf89fe5cd28ac804931b3cda447479a5
+U
+%src/main/java/com/sehan/TheLists.java,9\b\9b3ad4eba0001a574c2ca93c18207fea6498687a
+V
+&src/main/java/com/sehan/TheArrays.java,6\2\6245af4c06bbc14e51610a2c017d1f0d95056292
+V
+&src/main/java/com/sehan/TheStacks.java,a\0\a0e0cb3d1819e879d5dd7998e772bd8bfabf9bde
--- src/main/java/com/sehan/TheQueues.java
+++ src/main/java/com/sehan/TheQueues.java
@@ -1,10 +1,26 @@
 package com.sehan;
 
 import java.util.LinkedList;
+import java.util.ListIterator;
 import java.util.Queue;
 
 public class TheQueues {
     public static void main(String[] args) {
+        LinkedList<Person> linkedList = new LinkedList<>();
+        linkedList.add(new Person("Alex", 21));
+        linkedList.add(new Person("Alexa", 21));
+        linkedList.addFirst(new Person("Ali", 18));
+        ListIterator<Person> personListIterator = linkedList.listIterator();
+        while (personListIterator.hasNext()){
+            System.out.println(personListIterator.next());
+        }
+        System.out.println();
+        while (personListIterator.hasPrevious()){
+            System.out.println(personListIterator.previous());
+        }
+    }
+
+    private static void queues(){
         Queue<Person> queue = new LinkedList<>();
         queue.add(new Person("Alex", 21));
         queue.add(new Person("Mariam", 18));
@@ -16,5 +32,6 @@ public class TheQueues {
         System.out.println(queue.size());
         System.out.println(queue.peek());
     }
+
     static record Person(String name, int age){}
 }
