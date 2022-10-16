81f63b76066d2a483f406d0fdbaeb8d70d5e4a3a
sehan1 <sehan@openarcgroup.com>
2022-10-16 18:49:13 +0530

    The Sets

---
+++ src/main/java/com/sehan/TheSets.java
@@ -0,0 +1,46 @@
+package com.sehan;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class TheSets {
+    public static void main(String[] args) {
+        Set<Ball> balls = new HashSet<>();
+        balls.add(new Ball("blue"));
+        balls.add(new Ball("blue"));
+        balls.add(new Ball("yellow"));
+        balls.add(new Ball("red"));
+        balls.remove(new Ball("red"));
+        System.out.println(balls.size());
+        balls.forEach(System.out::println);
+    }
+    static class Ball{
+        String color;
+
+        public Ball(String color) {
+            this.color = color;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Ball ball = (Ball) o;
+            return Objects.equals(color, ball.color);
+        }
+
+        @Override
+        public String toString() {
+            return "Ball{" +
+                    "color='" + color + '\'' +
+                    '}';
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(color);
+        }
+    }
+//    record Ball(String color){}
+}
